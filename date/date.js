60b72b4bc08b97fa4afd2732d71a9d757835e278
Nikita <nikitaemrih226@gmail.com>
2025-09-13 12:03:47 +0300

    first commit

---
+++ date/date.js
@@ -0,0 +1,22 @@
+const moment = require('moment');
+
+function getCurrentDay() {
+    console.log(moment().format('dddd'));
+}
+
+function getCurrentMonth() {
+    console.log(moment().format('MMMM'));
+}
+
+function getCurrentYear() {
+    console.log(moment().format('YYYY'));
+}
+
+function getDate() {
+    console.log(moment().format('YYYY/MM/DD HH:mm:ss'));
+}
+
+getCurrentDay();
+getCurrentMonth();
+getCurrentYear();
+getDate();
